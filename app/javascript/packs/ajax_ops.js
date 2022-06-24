edf8a602a8fe22664b079660ccc79d3a157826f4
CeeFish <can.o.fisher@gmail.com>
2022-06-24 16:51:34 -0400

    Added buttons to html file and ops JS file.

--- app/javascript/packs/ajax_ops.js
+++ app/javascript/packs/ajax_ops.js
@@ -11,7 +11,14 @@ function handle_ajax(event) {
   const memberID = document.getElementById('member-id');
   const firstName1 = document.getElementById('member-firstName1');
   const lastName1 = document.getElementById('member-lastName1');
+  const deleteMemberButton = document.getElementById('delete-member');
+  const memberID1 = document.getElementById('member-id1');
+  const listFacts = document.getElementById('list-facts');
+  const memberID2 = document.getElementById('member-id2');
+  const createFactButton = document.getElementById('create-fact');
+  const memberID3 = document.getElementById('member-id3');
   const members_path = 'http://localhost:3001/api/v1/members';
+  const members_path2 = 'http://localhost:3001/api/v1/members/member_id/facts';
 
   restOpsDiv.addEventListener('click', (event) => {
     if (event.target === listMembersButton) {
@@ -70,6 +77,85 @@ function handle_ajax(event) {
       }
       fetch(`${members_path}/${memberID.value}`,
         { method: 'PUT',
+          headers: { 'Content-Type': 'application/json',
+            'authorization': authHeader },
+          body: JSON.stringify(dataObject)
+        }
+      ).then((response) => {
+        if (response.status === 200) {
+          response.json().then((data) => {
+            resultsDiv.innerHTML = '';
+            let parag = document.createElement('P');
+            parag.textContent = JSON.stringify(data);
+            resultsDiv.appendChild(parag);
+          });
+        } else {
+          response.json().then((data) => {
+            alert(`Return code ${response.status} ${response.statusText} ${JSON.stringify(data)}`);
+          }).catch((error) => {
+            console.log(error);
+            alert(error);
+          });
+        }
+      });
+    } else if (event.target === deleteMemberButton) {
+      var dataObject = {
+        member_ID1: memberID1.value
+      }
+      fetch(`${members_path}/${memberID1.value}`,
+        { method: 'DELETE',
+          headers: { 'authorization': authHeader }
+        }
+      ).then((response) => {
+        if (response.status === 200) {
+          response.json().then((data) => {
+            resultsDiv.innerHTML = '';
+            let parag = document.createElement('P');
+            parag.textContent = JSON.stringify(data);
+            resultsDiv.appendChild(parag);
+          });
+        } else {
+          response.json().then((data) => {
+            alert(`Return code ${response.status} ${response.statusText} ${JSON.stringify(data)}`);
+          }).catch((error) => {
+            console.log(error);
+            alert(error);
+          });
+        }
+      });
+    } else if (event.target === listFacts) {
+      var dataObject = {
+        member_ID2: memberID2.value
+      }
+      fetch(`${members_path2}/${memberID2.value}`,
+        { method: 'PUT',
+          headers: { 'Content-Type': 'application/json',
+            'authorization': authHeader },
+          body: JSON.stringify(dataObject)
+        }
+      ).then((response) => {
+        if (response.status === 200) {
+          response.json().then((data) => {
+            resultsDiv.innerHTML = '';
+            let parag = document.createElement('P');
+            parag.textContent = JSON.stringify(data);
+            resultsDiv.appendChild(parag);
+          });
+        } else {
+          response.json().then((data) => {
+            alert(`Return code ${response.status} ${response.statusText} ${JSON.stringify(data)}`);
+          }).catch((error) => {
+            console.log(error);
+            alert(error);
+          });
+        }
+      });
+    } else if (event.target === createFactButton) {
+      var dataObject = {
+        member_ID3: memberID3.value
+      }
+      fetch(`${members_path2}/${memberID3.value}`,
+        { method: 'POST',
           headers: { 'Content-Type': 'application/json',
             'authorization': authHeader },
           body: JSON.stringify(dataObject)
